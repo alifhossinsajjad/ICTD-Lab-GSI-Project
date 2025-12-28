25f9e9c83030742411cf19ed0071c09d78ff0dce
nosib55 <lmnosib555@gmail.com>
2025-12-28 23:44:16 +0600

    lab page added

--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -4,6 +4,7 @@ import RootLayout from "../layout/RootLayout";
 import ErrorPage from "../components/ErrorPage/ErrorPage";
 import Home from "../Pages/Home/Home/Home"
 import AllNotice from "../Pages/AllNotice/AllNotice";
+import Lab from "../Pages/lab/Lab";
 const router = createBrowserRouter([
   {
     path: "/",
@@ -17,6 +18,10 @@ const router = createBrowserRouter([
       {
         path: "all-notice",
         Component: AllNotice,
+      },
+      {
+        path: "lab",
+        Component: Lab,
       }
     ],
   },
