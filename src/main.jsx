d7160c23e6300ac3c3176d2b00bd7bc14c233e59
MD ALIF MIA <alifhossinsajjad123456@gmail.com>
2025-12-31 00:20:11 +0600

    delete file and conflict resolver

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./App.css";
-import App from "./App.jsx";
+import "./index.css";
 import { RouterProvider } from "react-router";
 import router from "./Routes/Router.jsx";
 import "./components/languages/language/i18n.js";
