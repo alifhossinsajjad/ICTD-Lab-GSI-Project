23051eb79310f845bc4d32e2157551b017205a3b
MD ALIF MIA <alifhossinsajjad123456@gmail.com>
2025-12-30 18:45:21 +0600

    import components in the home page

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,41 +1,31 @@
-
-import React from 'react';
-import Banner from '../Banner/Banner';
-import Contact from '../../Contact/Contact';
-import Vendor from '../Vendor/Vendor';
-
-
+import React from "react";
+import Banner from "../Banner/Banner";
+import Contact from "../../Contact/Contact";
+import Vendor from "../Vendor/Vendor";
+import AllNotice from "../../AllNotice/AllNotice";
+import Goals from "../Goals/Goals";
 
 const Home = () => {
-    return (
-        <div>
-            <section>
-          <Banner/>
-            </section>
+  return (
+    <div>
+      <section>
+        <Banner />
+      </section>
 
-            <section>
-                <Notice/>
-            </section>
-            <section>
-                <About/>
-            </section>
-
-     <section>
-                <AllNotice/>
-            </section>
-            <section>
-                <Goals/>
-            </section>
-            <section>
-               <Vendor/>
-            </section>
-            <section>
-                <Contact/>
-            </section>
-       
-           
-        </div>
-    );
+      <section>
+        <AllNotice/>
+      </section>
+      <section>
+        <Goals/>
+      </section>
+      <section>
+        <Vendor />
+      </section>
+      <section>
+        <Contact />
+      </section>
+    </div>
+  );
 };
 
-export default Home
+export default Home;
