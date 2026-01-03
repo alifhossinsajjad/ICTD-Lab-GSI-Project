7db43b4b24a7e06dabdf5523bc5fb8e12bea0b03
MD ALIF MIA <alifhossinsajjad123456@gmail.com>
2026-01-03 19:13:46 +0600

    all up to date

--- src/main.jsx
+++ src/main.jsx
@@ -14,7 +14,6 @@ const queryClient = new QueryClient();
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      
       <RouterProvider router={router} />
     </QueryClientProvider>
   </StrictMode>
